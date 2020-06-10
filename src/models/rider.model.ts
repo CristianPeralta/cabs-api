168e62ddd9bf6249556f356a6c00428faf371e3b
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-10 13:40:51 -0500

    create rider model

---
+++ src/models/rider.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Rider extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  gender: string;
+
+  @property({
+    type: 'string',
+  })
+  country?: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  dob: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  paymentType: string;
+
+
+  constructor(data?: Partial<Rider>) {
+    super(data);
+  }
+}
+
+export interface RiderRelations {
+  // describe navigational properties here
+}
+
+export type RiderWithRelations = Rider & RiderRelations;
